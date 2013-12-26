2ee0f8121707c72146d519c4f408897a79a87cc0
linbc <linbc01@gmail.com>
2013-12-26 18:28:00 +0800

    first commit

---
+++ buildConfig.h
@@ -0,0 +1,21 @@
+#ifndef _BUILD_CONFIG_H_
+#define _BUILD_CONFIG_H_
+
+#define BLD_PRODUCT "appweb"
+#define BLD_NAME "Mbedthis Appweb"
+#define BLD_VERSION "2.4.2"
+#define BLD_NUMBER "2"
+#define BLD_TYPE "DEBUG"
+#define BLD_DEFAULTS "standard"
+#define BLD_HOST_CPU "MPR_CPU_IX86"
+#define BLD_HOST_OS "WIN7"
+#define BLD_PREFIX "./"
+
+#define WIN 1
+#define BLD_FEATURE_NUM_TYPE int
+
+#define BLD_FEATURE_NUM_TYPE_ID 5
+#define BLD_FEATURE_MALLOC_HOOK 1
+
+
+#endif
